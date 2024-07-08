f95cb60172a8cfc3a4447cf8c44bfe8faba9e5e6
Frank Mwito <Mfranklyne039@gmail.com>
2024-07-08 17:16:35 +0300

    Unique set integers

---
+++ src/Common integers in two sets.kt
@@ -0,0 +1,23 @@
+import java.util.*
+
+fun main()  {
+    val reader = Scanner(System.`in`)
+    println("Enter the size of the first Set:")
+    val n1 = reader.nextInt()
+    val set1 = mutableSetOf<Int>()
+    println("Enter the size of the second set:")
+    val n2 = reader.nextInt()
+    val set2 = mutableSetOf<Int>()
+
+    for (i in 0 until n1){
+        println("Enter the values of the first set")
+        set1.add(reader.nextInt())
+    }
+
+    for (i in 0 until n2){
+        println("Enter the values of the second set:")
+        set2.add(reader.nextInt())
+    }
+    println("${set1.intersect(set2)}")
+
+}

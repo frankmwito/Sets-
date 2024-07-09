206435632155f52911b50751356ea8c8cfc1e5cd
Frank Mwito <Mfranklyne039@gmail.com>
2024-07-09 05:36:54 +0300

    Set equality

---
+++ src/Set equality.kt
@@ -0,0 +1,29 @@
+import java.util.Scanner
+
+fun main() {
+    val reader = Scanner(System.`in`)
+    println("Enter rhe size of the first set:")
+
+    val n1 = reader.nextInt()
+    val set1 = mutableSetOf<Int>()
+
+    for (i in 0 until n1){
+        println("Enter the values of the first set:")
+        set1.add(reader.nextInt())
+    }
+
+    println("Enter the size of the second set:")
+    val n2 = reader.nextInt()
+    val set2 = mutableSetOf<Int>()
+
+    for (i in 0 until n2){
+        println("Enter the values of the second set:")
+        set2.add(reader.nextInt())
+    }
+    if (set1 == set2){
+        println("The sets are equal")
+    }
+    else{
+        println("The sets are not equal")
+    }
+}
